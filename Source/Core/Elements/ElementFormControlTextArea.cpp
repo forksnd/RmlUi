forksnd/RmlUi

commit 4fd473bc5021f9fe886dda457394ada18042d7e6

Scale intrinsic sizes of elements by dp-ratio and line-height as appropriate (#917)

Source/Core/Elements/ElementFormControlTextArea.cpp
@@ -96,7 +96,7 @@ void ElementFormControlTextArea::SetCompositionRange(int range_start, int range_
 bool ElementFormControlTextArea::GetIntrinsicDimensions(Vector2f& dimensions, float& /*ratio*/)
 {
 	dimensions.x = (float)(GetNumColumns() * ElementUtilities::GetStringWidth(this, "m"));
-	dimensions.y = (float)GetNumRows() * Math::Round(GetLineHeight());
+	dimensions.y = GetNumRows() * GetLineHeight();
 
 	return true;
 }

Source/Core/Elements/InputTypeText.cpp
@@ -85,7 +85,7 @@ void InputTypeText::ProcessDefaultAction(Event& /*event*/) {}
 bool InputTypeText::GetIntrinsicDimensions(Vector2f& dimensions, float& /*ratio*/)
 {
 	dimensions.x = (float)(size * ElementUtilities::GetStringWidth(element, "m"));
-	dimensions.y = Math::Round(element->GetLineHeight());
+	dimensions.y = element->GetLineHeight();
 
 	return true;
 }

Source/Core/Elements/ElementFormControlSelect.cpp
@@ -146,8 +146,9 @@ void ElementFormControlSelect::MoveChildren()
 
 bool ElementFormControlSelect::GetIntrinsicDimensions(Vector2f& intrinsic_dimensions, float& /*ratio*/)
 {
-	intrinsic_dimensions.x = 128;
-	intrinsic_dimensions.y = 16;
+	intrinsic_dimensions.x = 128 * ElementUtilities::GetDensityIndependentPixelRatio(this);
+	intrinsic_dimensions.y = GetLineHeight();
+
 	return true;
 }
 

Source/Core/Elements/ElementProgress.cpp
@@ -52,8 +52,9 @@ void ElementProgress::SetValue(float in_value)
 
 bool ElementProgress::GetIntrinsicDimensions(Vector2f& dimensions, float& /*ratio*/)
 {
-	dimensions.x = 256;
-	dimensions.y = 16;
+	dimensions.x = 256 * ElementUtilities::GetDensityIndependentPixelRatio(this);
+	dimensions.y = GetLineHeight();
+
 	return true;
 }
 

Source/Core/Elements/InputTypeCheckbox.cpp
@@ -1,4 +1,5 @@
 #include "InputTypeCheckbox.h"
+#include "../../../Include/RmlUi/Core/ElementUtilities.h"
 #include "../../../Include/RmlUi/Core/Elements/ElementFormControlInput.h"
 
 namespace Rml {
@@ -50,6 +51,7 @@ bool InputTypeCheckbox::GetIntrinsicDimensions(Vector2f& dimensions, float& rati
 {
 	dimensions.x = 16;
 	dimensions.y = 16;
+	dimensions *= ElementUtilities::GetDensityIndependentPixelRatio(element);
 	ratio = 1;
 
 	return true;

Source/Core/Elements/InputTypeRadio.cpp
@@ -63,6 +63,7 @@ bool InputTypeRadio::GetIntrinsicDimensions(Vector2f& dimensions, float& ratio)
 {
 	dimensions.x = 16;
 	dimensions.y = 16;
+	dimensions *= ElementUtilities::GetDensityIndependentPixelRatio(element);
 	ratio = 1;
 
 	return true;

Source/Core/Elements/InputTypeRange.cpp
@@ -1,4 +1,5 @@
 #include "InputTypeRange.h"
+#include "../../../Include/RmlUi/Core/ElementUtilities.h"
 #include "../../../Include/RmlUi/Core/Elements/ElementFormControlInput.h"
 #include "WidgetSlider.h"
 
@@ -66,6 +67,7 @@ void InputTypeRange::ProcessDefaultAction(Event& /*event*/) {}
 bool InputTypeRange::GetIntrinsicDimensions(Vector2f& dimensions, float& /*ratio*/)
 {
 	widget->GetDimensions(dimensions);
+	dimensions *= ElementUtilities::GetDensityIndependentPixelRatio(element);
 	return true;
 }
 

commit e697f8f955f5bdff40687aa9b01f342b232948ac

Fix multi-line text fields possibly generating scrollbars when intrinsically sized
Intrinsic size of text fields now uses the rounded line height, which resolves a mismatch between intrinsic and formatted line size.

Resolves a regression from #917.

Source/Core/Elements/ElementFormControlTextArea.cpp
@@ -96,8 +96,7 @@ void ElementFormControlTextArea::SetCompositionRange(int range_start, int range_
 bool ElementFormControlTextArea::GetIntrinsicDimensions(Vector2f& dimensions, float& /*ratio*/)
 {
 	dimensions.x = (float)(GetNumColumns() * ElementUtilities::GetStringWidth(this, "m"));
-	dimensions.y = GetNumRows() * GetLineHeight();
-
+	dimensions.y = GetNumRows() * widget->GetLineHeight();
 	return true;
 }
 

Source/Core/Elements/InputTypeText.cpp
@@ -85,8 +85,7 @@ void InputTypeText::ProcessDefaultAction(Event& /*event*/) {}
 bool InputTypeText::GetIntrinsicDimensions(Vector2f& dimensions, float& /*ratio*/)
 {
 	dimensions.x = (float)(size * ElementUtilities::GetStringWidth(element, "m"));
-	dimensions.y = element->GetLineHeight();
-
+	dimensions.y = widget->GetLineHeight();
 	return true;
 }
 

Source/Core/Elements/WidgetTextInput.cpp
@@ -1622,6 +1622,8 @@ void WidgetTextInput::SetKeyboardActive(bool active)
 
 float WidgetTextInput::GetLineHeight() const
 {
+	// This is rounded to improve the navigation experience in a multi-line text field. If it's not rounded then lines
+	// may move a single pixel for every line, which looks a bit jaggedy when moving up and down.
 	return Math::Round(parent->GetLineHeight());
 }
 

Source/Core/Elements/WidgetTextInput.h
@@ -71,6 +71,9 @@ public:
 	/// Force text formatting on the next layout update.
 	void ForceFormattingOnNextLayout();
 
+	/// Returns the used line height.
+	float GetLineHeight() const;
+	
 	/// Updates the cursor, if necessary.
 	void OnUpdate();
 	/// Renders the cursor, if it is visible.
@@ -228,8 +231,6 @@ private:
 	/// Returns the offset that aligns the contents of the line according to the 'text-align' property.
 	float GetAlignmentSpecificTextOffset(const Line& line) const;
 
-	/// Returns the used line height.
-	float GetLineHeight() const;
 	/// Returns the width available for the text contents without overflowing, that is, the content area subtracted by any scrollbar.
 	float GetAvailableWidth() const;
 	/// Returns the height available for the text contents without overflowing, that is, the content area subtracted by any scrollbar.